7110eeada2c243e57825a706f37248311f2c293d
unknown <you@example.com>
2018-03-19 16:40:07 +0800

    'mm'

--- src/components/FooterGuide/FooterGuide.vue
+++ src/components/FooterGuide/FooterGuide.vue
@@ -1,18 +1,29 @@
 <template>
-    <div>
-      <a href="javascript:" @click="goto('/Msite')">
-        <i class="iconfont icon-shouyedianpujishishangcheng"></i>
-        首页
-      </a>
-      <a href="javascript:" @click="goto('/Type')">
-        <i class="iconfont icon-fenlei"></i>
-        分类</a>
-      <a href="javascript:" @click="goto('/Cart')">
-        <i class="iconfont icon-shopshoppingco"></i>
-        购物车</a>
-      <a href="javascript:" @click="goto('/Myepet')">
-        <i class="iconfont icon-icon-test"></i>
-        我的E宠</a>
+    <div class="footer_guide border-1px">
+      <span class="guide_item" :class="{on: isCurrent('/Msite')}" @click="goto('/Msite')">
+        <span>
+          <i class="iconfont icon-shouyedianpujishishangcheng"></i>
+        </span>
+        <span>首页</span>
+      </span>
+      <span class="guide_item" :class="{on: isCurrent('/Type')}" @click="goto('/Type')">
+        <span>
+          <i class="iconfont icon-fenlei"></i>
+        </span>
+        <span>分类</span>
+      </span>
+      <span class="guide_item" :class="{on: isCurrent('/Cart')}" @click="goto('/Cart')">
+        <span>
+          <i class="iconfont icon-shopshoppingco"></i>
+        </span>
+        <span>购物车</span>
+      </span>
+      <span class="guide_item" :class="{on: isCurrent('/Myepet')}" @click="goto('/Myepet')">
+        <span>
+          <i class="iconfont icon-icon-test"></i>
+        </span>
+        <span>我的E宠</span>
+      </span>
     </div>
 </template>
 <script>
@@ -20,10 +31,36 @@
         methods:{
           goto(path){
               this.$router.replace(path)
+          },
+          isCurrent(path){
+              return this.$route.path === path
           }
         }
     }
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
-
+  .footer_guide  //footer
+    top-border-1px(#e4e4e4)
+    position fixed
+    z-index 100
+    left 0
+    right 0
+    bottom 0
+    background-color #fff
+    width 100%
+    height 50px
+    display flex
+    .guide_item
+      display flex
+      flex 1                // 每个分成一等分
+      flex-direction column //垂直排列
+      align-items center    //字体居中
+      margin 5px
+      color #999999
+      &.on
+        color red
+      span
+        font-size 13px
+        .iconfont
+          font-size 25px
 </style>
--- src/pages/Msite/Msite.vue
+++ src/pages/Msite/Msite.vue
@@ -1,11 +1,158 @@
 <template>
-    <div>
-        Msite
+<div>
+  <header>
+    <div class="headerbox">
+      <div class="headermain">
+        <div class="headerup">
+          <div class="nekobashou">
+            <span>ねこ</span>
+            <span>|</span>
+            <span>重庆</span>
+            <span></span>
+          </div>
+          <div class="headerseach">
+            <input type="text" placeholder="搜索商品和品牌">
+            <i class="iconfont icon-sousuo"></i>
+          </div>
+          <div>
+            <i class="iconfont icon-msnui-sms-bubble"></i>
+          </div>
+        </div>
+      </div>
     </div>
+    <div class="headerbox2">
+      <div class="swiper-container banner">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide">
+            <p>首页</p>
+          </div>
+          <div class="swiper-slide">
+            <p>猫猫主粮</p>
+          </div>
+          <div class="swiper-slide">
+            <p>罐头超市</p>
+          </div>
+          <div class="swiper-slide maosha">
+            <p>猫砂</p>
+          </div>
+          <div class="swiper-slide">
+            <p>医疗保健</p>
+          </div>
+          <div class="swiper-slide">
+            <p>美容香波</p>
+          </div>
+          <div class="swiper-slide">
+            <p>零食玩具</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+  <div class="swiper-container case">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide">
+        <img src="./images/9f49bbf054e4dcab567e1584f48db3f0.jpg" alt="">
+      </div>
+      <div class="swiper-slide">
+        <img src="./images/94d4681e2ab878c340d441a5c1c7b98a.jpg" alt="">
+      </div>
+      <div class="swiper-slide">
+        <img src="./images/b898562338c7f04ac9b2fa3e5aec8b4c.jpg" alt="">
+      </div>
+    </div>
+    <div class="swiper-pagination case"></div>
+  </div>
+  <div class="homecontainer">
+    <div class="columnnavdiv">
+      <ul>
+        <li><img src="./images/echongtuan.jpg" alt=""></li>
+        <li><img src="./images/echongtuan.jpg" alt=""></li>
+        <li><img src="./images/echongtuan.jpg" alt=""></li>
+        <li><img src="./images/echongtuan.jpg" alt=""></li>
+        <li><img src="./images/echongtuan.jpg" alt=""></li>
+        <li><img src="./images/echongtuan.jpg" alt=""></li>
+        <li><img src="./images/echongtuan.jpg" alt=""></li>
+        <li><img src="./images/echongtuan.jpg" alt=""></li>
+        <li><img src="./images/echongtuan.jpg" alt=""></li>
+        <li><img src="./images/echongtuan.jpg" alt=""></li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+
 </template>
 <script>
-    export default {}
+    export default {
+      mounted(){
+        const swiper = new Swiper('.case', {
+          pagination: {
+            el: '.case .swiper-pagination',
+            loop:true,
+            autoplay: {
+              delay: 3000,
+              stopOnLastSlide: false,
+              disableOnInteraction: true,
+            }
+          }
+        });
+        const myswiper = new Swiper('.banner', {
+          slidesPerView: 5,
+          spaceBetween: 30,
+        });
+      }
+    }
 </script>
-<style lang="stylus" rel="stylesheet/stylus">
 
+<style lang="stylus" rel="stylesheet/stylus">
+  .headerbox
+    height 86px
+    .headermain
+      box-sizing border-box
+      height 51px
+      padding 5px 10px
+      .headerup
+        display flex
+        justify-content space-around
+        box-sizing border-box
+        height 41px
+        padding 8px 0
+        white-space nowrap
+        .nekobashou
+        .headerseach
+          .iconfont
+            margin-left: -22px;
+
+  .headerbox2
+    position: relative;
+    top: -20px;
+    font-size 14px
+    margin-bottom: -11px;
+    >.swiper-container
+      >.swiper-wrapper
+        white-space: nowrap
+        >.swiper-slide
+          text-align center
+        >.swiper-slide:eq(4)
+          margin-right 15px
+
+
+  .swiper-container.case
+    width 100%
+    height 160px
+    .swiper-wrapper
+      .swiper-slide
+        width 100%
+        height 100%
+        img
+          width 100%
+          height 100%
+  .homecontainer
+    width 100%
+    height 180px
+    .columnnavdiv
+      width 100%
+      height 100%
+      ul
+        float left
 </style>
